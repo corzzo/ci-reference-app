195c604a765463b969b6c0356541e683ab963086
Juan Corzo <jcorzo@apple.com>
2018-04-06 13:32:35 -0400

    added testing

--- client/store/shas/__tests__/index.js
+++ client/store/shas/__tests__/index.js
@@ -1,16 +1,22 @@
 
 import { createStore } from 'redux'
-// import assert from 'assert'
+import assert from 'assert'
 
 import reducer from '../reducer'
 import { createSha } from '..'
 
+global.fetch = require('jest-fetch-mock')
+
 test('update images', async () => {
-  // do stuff
+  fetch.once(JSON.stringify({
+    sha: 'asdf'
+  }))
 
   const store = createStore(reducer)
-  const sha = await createSha(store.dispatch)('1234')
 
-  // do stuff
-  console.log(sha) // so lint passes
+  const sha = await createSha(store.dispatch)('1234')
+  assert.equal(sha, 'asdf')
+
+  const shas = store.getState()
+  assert(shas.some(x => x === 'asdf'))
 })
